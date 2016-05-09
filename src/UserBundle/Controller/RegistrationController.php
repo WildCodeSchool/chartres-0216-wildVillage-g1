c286341f1d9ffb68e3e2d5e04860f1fdaf6c3071
etidimjav <thomas.javanaud@gmail.com>
2016-05-09 15:52:47 +0200

    MAJ redirection enregistrement vers actu

--- src/UserBundle/Controller/RegistrationController.php
+++ src/UserBundle/Controller/RegistrationController.php
@@ -63,7 +63,7 @@ class RegistrationController extends Controller
             $userManager->updateUser($user);
 
             if (null === $response = $event->getResponse()) {
-                $url = $this->generateUrl('fos_user_registration_confirmed');
+                $url = $this->generateUrl('actu_homepage');
                 $response = new RedirectResponse($url);
             }
 
