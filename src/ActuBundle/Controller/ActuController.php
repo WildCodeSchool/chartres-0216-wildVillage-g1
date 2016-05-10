5db0f9ea8fdc4b33cb354eefbc96ac846b91cb71
etidimjav <thomas.javanaud@gmail.com>
2016-05-10 12:04:17 +0200

    Gestion sidebar amis

--- src/ActuBundle/Controller/ActuController.php
+++ src/ActuBundle/Controller/ActuController.php
@@ -3,11 +3,26 @@
 namespace ActuBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use UserBundle\Entity\User;
 
 class ActuController extends Controller
 {
     public function actuAction()
     {
         return $this->render('ActuBundle:Actu:actu.html.twig');
+    }
+
+    public function sidebarAction (Request $request)
+    {
+        $user = $this -> getUser();
+
+        $repository = $this->getDoctrine()->getRepository('UserBundle:User');
+ 
+        $sidebar_user = $repository->findAll();;
+
+				return $this->render('sidebar/sidebar.html.twig', array(
+					'sidebar_user' => $sidebar_user,
+        ));	
     }
 }
